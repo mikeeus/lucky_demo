379fc7d6c1311ab88e2d095f9e1b3ad52a31be26
mikeeus <itsmikias@gmail.com>
2018-07-27 12:35:16 +0300

    Render component on homepage

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -1,6 +1,16 @@
 class Home::IndexPage < GuestLayout
+  include React::Component
+
   def content
     h1 "React Component"
-    tag "component"
+
+    messages = [
+      { id: 1, sender: "me", text: "Hi" },
+      { id: 2, sender: "Chatbot", text: "Hi! How can I help?" },
+      { id: 3, sender: "me", text: "Can you tell me the time?" },
+      { id: 4, sender: "Chatbot", text: "Sure it's #{Time.now}" }
+    ]
+
+    react "Chat", { messages: messages }
   end
 end
