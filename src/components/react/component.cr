e85ce07367a939c921d27a78271335688b84e951
mikeeus <itsmikias@gmail.com>
2018-07-27 14:37:32 +0300

    Add support for wrapping components

--- src/components/react/component.cr
+++ src/components/react/component.cr
@@ -3,7 +3,19 @@ module React::Component
     tag tag, "data-react-class": class_name
   end
 
+  def react(class_name : String, tag : String? = "div", &block)
+    tag tag, "data-react-class": class_name do
+      yield
+    end      
+  end
+
   def react(class_name : String, props : NamedTuple | JSON::Any, tag : String? = "div")
     tag tag, "data-react-class": class_name, "data-react-props": props.to_json
+  end
+
+  def react(class_name : String, props : NamedTuple | JSON::Any, tag : String? = "div", &block)
+    tag tag, "data-react-class": class_name do
+      yield
+    end      
   end
 end
