8ab5b4af04cea7087bb7bb338ca30e878f0116fa
mikeeus <itsmikias@gmail.com>
2018-07-29 11:41:26 +0300

    Change error message

--- src/forms/image_form.cr
+++ src/forms/image_form.cr
@@ -17,7 +17,7 @@ class ImageForm < Image::BaseForm
   def prepare
     validate_is_correct_size
     validate_is_correct_dimensions
-    validate_is_correct_format
+    validate_is_correct_type
 
     if errors.empty?
       save_image
@@ -28,11 +28,11 @@ class ImageForm < Image::BaseForm
     end
   end
 
-  private def validate_is_correct_format
+  private def validate_is_correct_type
     ext = crymagick_image.type
 
     unless Image::VALID_FORMATS.includes? "#{ext}".downcase
-      image.add_error "extension should be jpg, jpeg, gif or png but was #{ext}"
+      image.add_error "type should be jpg, jpeg, gif or png but was #{ext}"
     end
   end
 
