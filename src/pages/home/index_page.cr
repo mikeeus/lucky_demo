9e81f9a3fe84bfaaa8804216d4d6232e5eb050af
mikeeus <itsmikias@gmail.com>
2018-07-29 10:46:03 +0300

    Display errors

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -7,7 +7,13 @@ class Home::IndexPage < GuestLayout
 
   private def render_form(f)
     form_for Images::Create, enctype: "multipart/form-data" do
-      text_input f.image, type: "file", flow_id: "file-input"
+      text_input f.image, type: "file", flow_id: "file-input"      
+
+      ul do
+        f.image.errors.each do |err|
+          li "Image #{err}", class: "error"
+        end
+      end
 
       submit "Upload Image", flow_id: "upload-image"
     end
