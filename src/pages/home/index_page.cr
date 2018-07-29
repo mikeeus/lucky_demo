74a038093a9aa2927abf680295aed1548af4b965
mikeeus <itsmikias@gmail.com>
2018-07-29 13:15:45 +0300

    Add gallery

--- src/pages/home/index_page.cr
+++ src/pages/home/index_page.cr
@@ -1,8 +1,33 @@
 class Home::IndexPage < GuestLayout
   needs form : ImageForm
+  needs images : ImageQuery
 
   def content
-    render_form(@form)
+    div class: "homepage-container" do
+      render_form(@form)
+      
+      gallery
+    end
+  end
+
+  private def gallery
+    h2 "Image Gallery"
+
+    ul class: "image-gallery" do
+      @images.map do |image|
+        li class: "image", flow_id: "image-#{image.id}" do
+          div class: "picture", style: "background-image: url(#{image.path});" do
+            div "Views: #{image.views}", class: "views"
+          end
+
+          link to: Images::Delete.with(image.id), flow_id: "delete-image-#{image.id}" do
+            img src: asset("images/x.png")
+          end
+
+          div image.url, class: "image-url",  flow_id: "image-url-#{image.id}"
+        end
+      end
+    end
   end
 
   private def render_form(f)
